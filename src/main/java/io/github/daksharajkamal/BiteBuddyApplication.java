dca148b94d574544c8dab0d5a43222e910a06c66
Daksharaj Kamal <daksharajkamal111@gmail.com>
2023-04-21 16:42:39 +0530

    Initial Project Setup

---
+++ src/main/java/io/github/daksharajkamal/BiteBuddyApplication.java
@@ -0,0 +1,13 @@
+package io.github.daksharajkamal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/daksharajkamal/BiteBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daksharajkamal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
